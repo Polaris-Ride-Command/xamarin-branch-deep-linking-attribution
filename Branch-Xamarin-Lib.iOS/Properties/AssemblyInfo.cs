364471acdd64eb5db3b5c5ff18a7bd5f822efa61
Tom Soderling <>
2022-12-06 12:41:15 -0600

    Retargeting for iOS and Android Lib projects, and iOS SDK project

--- Branch-Xamarin-Lib.iOS/Properties/AssemblyInfo.cs
+++ Branch-Xamarin-Lib.iOS/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using Foundation;
 // When the attribute is present, the linker—if enabled—will process the assembly
 // even if you’re using the “Link SDK assemblies only” option, which is the default for device builds.
 
-[assembly: LinkerSafe]
+[assembly: System.Reflection.AssemblyMetadata("IsTrimmable", "True")]
 
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
@@ -25,7 +25,7 @@ using Foundation;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
